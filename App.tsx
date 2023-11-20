9b749b9c575b6dbfcd3efcfb7a658630dc76548b
levisstrauss <levisstrauss11@yahoo.com>
2023-11-20 16:15:46 -0500

    Set up the theme and fonts

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.root}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    marginTop: 30,
+  },
+});
+export default App;
